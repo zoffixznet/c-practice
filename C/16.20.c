b9a25a71e5f5e5b8ab60f4f4c08b8cfd8d46d752
Zoffix Znet <cpan@zoffix.com>
2017-12-06 13:36:37 -0500

    x

---
+++ C/16.20.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    enum { NORTH, SOUTH, EAST, WEST } direction;
+
+    printf("%d\n", direction);
+    // int x = 0, y = 0;
+    // switch (direction
+
+    return 0;
+}
