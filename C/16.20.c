c8b40ff2d7d2d60dd4ceca74e7e798949b45b042
Zoffix Znet <cpan@zoffix.com>
2017-12-06 13:38:23 -0500

    x

--- C/16.20.c
+++ C/16.20.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-    enum { NORTH, SOUTH, EAST, WEST } direction;
+    enum { NORTH, SOUTH, EAST, WEST } direction = SOUTH;
 
-    printf("%d\n", direction);
-    // int x = 0, y = 0;
-    // switch (direction
+    int x = 0, y = 0;
+    switch (direction) {
+        case NORTH: y--; break;
+        case SOUTH: y++; break;
+        case WEST:  x--; break;
+        case EAST:  x++; break;
+    }
+    printf("%d, %d, %d\n", direction, x, y);
 
     return 0;
 }
