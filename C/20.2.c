4f51dff261e11e4107700f237adc0271a6891656
Zoffix Znet <cpan@zoffix.com>
2018-04-14 06:13:47 -0400

    x

--- C/20.2.c
+++ C/20.2.c
@@ -5,9 +5,11 @@ void print_binary(void *start, size_t size) {
     unsigned char *p = start;
     int line = 0;
     while (size--) {
-        for (int i = 7; i >= 0; i--)
+        for (int i = 7; i >= 0; i--) {
+            if (i == 3) putchar('|');
             putchar(*p & 1 << i ? '1' : '0');
-        putchar(' ');
+        }
+        printf("  ");
         p++;
         if ((++line % 10) == 0) putchar('\n');
     }
@@ -15,7 +17,10 @@ void print_binary(void *start, size_t size) {
 }
 
 int main(void) {
-    unsigned char i = 0;
-    PRINTB(i);
+    unsigned char b = 0;
+    for (int i = 0; i < 10; i++) {
+        PRINTB(b);
+        b ^= 1 << 3;
+    }
     return 0;
 }
