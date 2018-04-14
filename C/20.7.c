825b6bc29c760fe5441c38b60e9f74f748d2babc
Zoffix Znet <cpan@zoffix.com>
2018-04-14 18:48:50 -0400

    x

---
+++ C/20.7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+#define PRINTB(x) (print_binary((&x), sizeof(x)))
+void print_binary(void *start, size_t size) {
+    unsigned char *p = start;
+    int line = 0;
+    while (size--) {
+        for (int i = 7; i >= 0; i--) {
+            if (i == 3) putchar('|');
+            putchar(*p & 1 << i ? '1' : '0');
+        }
+        printf("  ");
+        p++;
+        if ((++line % 10) == 0) putchar('\n');
+    }
+    putchar('\n');
+}
+
+unsigned int rotate_left(unsigned int i, int n) {
+}
+unsigned int rotate_left(unsigned int i, int n) {
+}
+
+int main(void) {
+    unsigned int i = 12131242142;
+    PRINTB(i);
+    // i = swap_bytes(i);
+    PRINTB(i);
+    return 0;
+}
