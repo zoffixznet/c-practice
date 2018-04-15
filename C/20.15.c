fc6d25087947518a43f1f3291acfbec14d71ddba
Zoffix Znet <cpan@zoffix.com>
2018-04-14 22:23:35 -0400

    x

---
+++ C/20.15.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+#define PRINTB(x) (print_binary((&x), sizeof(x)))
+void print_binary(void *start, size_t size) {
+    unsigned char *p = start;
+    int line = 0;
+    while (size--) {
+        for (int i = 7; i >= 0; i--) {
+            if (i == 3) putchar('|');
+            putchar(*p & 1 << i ? '1' : '0');
+        }
+        printf("  ");
+        p++;
+        if ((++line % 10) == 0) putchar('\n');
+    }
+    putchar('\n');
+}
+
+int main(void) {
+    struct {
+        unsigned int flag:  1; // make the motherfucka unsigned
+    } s;
+    s.flag = 1;
+    PRINTB(s);
+    printf("%d\n", s.flag);
+    return 0;
+}
